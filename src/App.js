7a65101608e4f5d2ab7c19296e79afbb3d88082a
Leonardo Genzolini <leonardogenzolini@gmail.com>
2023-12-02 13:53:05 +0100

    adding calendar to the page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,87 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BpkCode } from '@skyscanner/backpack-web/bpk-component-code';
 import BpkButton from '@skyscanner/backpack-web/bpk-component-button';
 import BpkText from '@skyscanner/backpack-web/bpk-component-text';
-
+import BpkCalendar, {
+  CALENDAR_SELECTION_TYPE,
+} from '@skyscanner/backpack-web/bpk-component-calendar';
+import BpkInput, {
+  INPUT_TYPES,
+} from '@skyscanner/backpack-web/bpk-component-input';
+import format from 'date-fns/format';
 import { cssModules } from '@skyscanner/backpack-web/bpk-react-utils';
 
 import STYLES from './App.scss';
 
 const getClassName = cssModules(STYLES);
+const formatDateFull = (date) => format(date, 'EEEE, do MMMM yyyy');
+const formatMonth = (date) => format(date, 'MMMM yyyy');
+const daysOfWeek = [
+  {
+    name: 'Sunday',
+    nameAbbr: 'Sun',
+    index: 0,
+    isWeekend: true,
+  },
+];
+
+class Calendar extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      selectionConfiguration: {
+        type: CALENDAR_SELECTION_TYPE.single,
+        date: null,
+      },
+    };
+  }
+
+  handleDateSelect = (date) => {
+    this.setState({
+      selectionConfiguration: {
+        type: this.props.selectionConfiguration.type,
+        date: date,
+      },
+    });
+  };
+
+  handleInputChange = (event) => {
+    this.setState({
+      selectionConfiguration: {
+        ...this.state.selectionConfiguration,
+        date: event.target.value,
+      },
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <BpkInput
+          id="dateInput"
+          type={INPUT_TYPES.text}
+          name="date"
+          value={(this.state.selectionConfiguration.date || '').toString()}
+          onChange={this.handleInputChange}
+          placeholder="Departure date"
+        />
+        <BpkCalendar
+          id="calendar"
+          onDateSelect={this.handleDateSelect}
+          formatMonth={formatMonth}
+          formatDateFull={formatDateFull}
+          daysOfWeek={daysOfWeek}
+          weekStartsOn={1}
+          changeMonthLabel="Change month"
+          nextMonthLabel="Next month"
+          previousMonthLabel="Previous month"
+          selectionConfiguration={this.state.selectionConfiguration}
+        />
+      </div>
+    );
+  }
+}
 
 const App = () => (
   <div className={getClassName('App')}>
@@ -17,10 +91,8 @@ const App = () => (
       </div>
     </header>
     <main className={getClassName('App__main')}>
-      <BpkText tagName="p" className={getClassName('App__text')}>
-        To get started, edit <BpkCode>src/App.jsx</BpkCode> and save to reload.
-      </BpkText>
-      <BpkButton onClick={() => alert('It works!')}>Click me</BpkButton>
+      <Calendar />
+      <BpkButton onClick={() => alert('It works!')}>Continue</BpkButton>
     </main>
   </div>
 );
